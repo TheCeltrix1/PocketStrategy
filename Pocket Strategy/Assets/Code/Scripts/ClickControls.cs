76d58a094eecd7621ef6a51ae08f4fb77d898a33
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2020-09-23 22:53:37 +0100

    Basic Movement
    A basic point a click movement system from a fixed camera that can toggle which robot is moving.

---
+++ Pocket Strategy/Assets/Code/Scripts/ClickControls.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickControls : MonoBehaviour
+{
+    public ScriptableObjects robots;
+    public GameObject[] robotGameObjects;
+
+    public Camera _cam;
+
+    private int _selectedRobot = 0;
+    private RaycastHit _hit;
+    private Ray _ray;
+    private NavMeshHit _navMeshHit;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            _ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray, out _hit))
+            {
+                if (NavMesh.SamplePosition(_hit.point, out _navMeshHit, 2, NavMesh.AllAreas))
+                {
+                    Move robbie = robotGameObjects[_selectedRobot].GetComponent<Move>();
+                    robbie.desitination = _navMeshHit.position;
+                    robbie.moveToPosition = true;
+                }
+            }
+        }
+        if (Input.GetButtonDown("NextRobot") || Input.GetButtonDown("PreviousRobot")) {
+            if (Input.GetButtonDown("NextRobot")) _selectedRobot++;
+            else if (Input.GetButtonDown("PreviousRobot")) _selectedRobot --;
+
+            if (_selectedRobot < 0)
+            {
+                _selectedRobot = robotGameObjects.Length - 1;
+            }
+            else if (_selectedRobot >= robotGameObjects.Length)
+            {
+                _selectedRobot = 0;
+            }
+        }
+    }
+}
---
+++ Pocket Strategy/Assets/Code/Scripts/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+    public bool moveToPosition = false;
+    public Vector3 desitination;
+    public int speed;
+
+    private NavMeshAgent _navMesh;
+
+    void Start()
+    {
+        _navMesh = this.transform.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveToPosition == true)
+        {
+            _navMesh.Move((desitination - transform.position) * Time.deltaTime);
+        }
+        if (Vector3.Distance(transform.position,desitination) < 1)
+        {
+            moveToPosition = false;
+        }
+    }
+}
