8f86f2d81ca6ef726b79a6242f96bc82bf06054e
JoshKearns <61230934+JoshKearns@users.noreply.github.com>
2020-09-27 00:57:01 +0100

    Light Switch Test

---
+++ Pocket Strategy/Assets/LightSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public GameObject lights;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lights.SetActive(!lights.activeSelf);
+        }
+    }
+}
