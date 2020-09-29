c2ef6cb9ac9300c434a67b517c8fdb90e3507287
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2020-09-29 19:14:54 +0100

    Testv2

---
+++ Pocket Strategy/Assets/Code/Scripts/ChargingStation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargingStation : MonoBehaviour
+{
+    private float _time;
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+    }
+}
